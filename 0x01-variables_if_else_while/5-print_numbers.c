0021435b8c1742ca49f9784a5930acfb6c74a558
Henok-Haile <henokhaile53@gmail.com>
2023-02-17 12:23:19 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers upto ten
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+	printf("%i", a);
+	a++;
+	}
+	printf("\n");
+	return (0);
+}
