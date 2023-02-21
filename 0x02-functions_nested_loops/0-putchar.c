8a01061e446ac757066d5c41bc09baf17696707b
Henok-Haile <henokhaile53@gmail.com>
2023-02-21 11:53:21 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+        return (0);
+	return (0);
+}
