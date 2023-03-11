4dc757969bafe611c79b2035a5cf062b4632b13d
Henok-Haile <henokhaile53@gmail.com>
2023-03-10 23:32:51 -0800

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * @argc: argument counter
+ * @argv: pointer to charchter
+ * Return: Always 0.
+ */
+
 int main(int argc, char *argv[])
 {
 	(void) argc;
