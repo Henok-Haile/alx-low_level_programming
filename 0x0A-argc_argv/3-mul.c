f76f72be9aa5f14f15e7d66f7412fa7af3791085
Henok-Haile <henokhaile53@gmail.com>
2023-03-11 00:31:45 -0800

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point.
+ *
+ * @argc: argument counter
+ * @argv: pointer to string
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(*(argv + 1)) * atoi(*(argv + 2)));
+		return (1);
+	}
+	else
+		printf("Error\n");
+	return (0);
+}
