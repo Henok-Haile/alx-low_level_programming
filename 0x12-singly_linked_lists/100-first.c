f43311c3127f6621275a20a875ddc695186255f1
Henok-Haile <henokhaile53@gmail.com>
2023-03-30 14:56:56 -0700

    Task 5 Advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print - prints before main
+ *
+ * Return: void.
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
