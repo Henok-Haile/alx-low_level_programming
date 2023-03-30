8ac90f1ca5cf3a886d1b9a841c105a7563763d99
Henok-Haile <henokhaile53@gmail.com>
2023-03-30 10:49:32 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,20 @@
  */
 size_t print_list(const list_t *h)
 {
+
 	int count = 0;
-
-	if (h == NULL)
-	{
-		printf("(nil)");
-	}
-
+	
 	while (h != NULL)
 	{
-		count++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			count++;
+		}
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		count++;
 	}
 	return (count);
 }
