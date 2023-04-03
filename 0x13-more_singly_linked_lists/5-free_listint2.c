dfeccad6d91a50d7967e219285c9f8e752808425
Henok-Haile <henokhaile53@gmail.com>
2023-04-03 09:00:02 -0700

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 
+	if (head == NULL)
+		return;
 	while (temp != NULL)
 	{
 		temp = temp->next;
 		free(*head);
 		*head = temp;
 	}
-	*head = NULL;
+	head = NULL;
 }
