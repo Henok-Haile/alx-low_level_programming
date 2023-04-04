9deec88026f4d0eb697ac709013c4d480904c14a
Henok-Haile <henokhaile53@gmail.com>
2023-04-04 02:38:37 -0700

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,8 +11,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *curr, *prev;
 
-	if (*head == NULL)
-		return (-1);
+	/*if (*head == NULL)
+		return (-1);*/
 
 	curr = *head;
 	prev = *head;
