ddfd736dd46f0bb7a6ce3336552fc523e499aaa5
Henok-Haile <henokhaile53@gmail.com>
2023-05-22 11:25:35 -0700

    Task 2 Advanced

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - adds two numbers
+ * @a: number
+ * @b: number
+ * Return: sum of numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: number
+ * @b: number
+ * Return: difference of numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: number
+ * @b: number
+ * Return: multiplication of numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: number
+ * @b: number
+ * Return: division of numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modules two numbers
+ * @a: number
+ * @b: number
+ * Return: module of numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
