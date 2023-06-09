69ac918c108848e3dfb8078b170823f37b4a6252
Henok-Haile <henokhaile53@gmail.com>
2023-06-09 09:22:03 -0700

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,26 +12,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 	unsigned long int i;
 
-	if (size <= 0)
-	{
-		return (NULL);
-	}
-
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
-	{
 		return (NULL);
-	}
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * ht->size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
-	{
-		free(ht);
 		return (NULL);
-	}
-
-	for (i = 0; i < ht->size; i++)
+	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
 
 	return (ht);
